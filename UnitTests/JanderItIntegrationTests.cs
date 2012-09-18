cc24cfabb88dec385968bf157fdb0988baf5bec7
Philip Jander (Jander.IT) <jander@janso.de>
2012-09-18 10:00:42 +0200

    Test for bug in original 2.0.5

--- UnitTests/JanderItIntegrationTests.cs
+++ UnitTests/JanderItIntegrationTests.cs
@@ -82,43 +82,73 @@ namespace UnitTests
         [Test]
         public void UsingGlobalsBug_singlethread_ok()
         {
-            var a = new ConcurrentClassA { PayloadA = new PayloadA() };
-            var b = new ConcurrentClassB { PayloadB = new PayloadB() };
+            string jsonA;
+            string jsonB;
+            GenerateJsonForAandB(out jsonA, out jsonB);
 
-            var jsonA = JSON.Instance.ToJSON(a, new JSONParameters { EnableAnonymousTypes = false, IgnoreCaseOnDeserialize = false, SerializeNullValues = false, ShowReadOnlyProperties = false, UseExtensions = true, UseFastGuid = false, UseOptimizedDatasetSchema = false, UseUTCDateTime = false, UsingGlobalTypes = true });
-            var jsonB = JSON.Instance.ToJSON(b);
-
-            Console.WriteLine(jsonA);
-            Console.WriteLine(jsonB);
-
-            var ax = JSON.Instance.ToObject(jsonA);
+            var ax = JSON.Instance.ToObject(jsonA); // A has type information in JSON-extended
+            var bx = JSON.Instance.ToObject<ConcurrentClassB>(jsonB); // B needs external type info
+            
             Assert.IsNotNull(ax);
             Assert.IsInstanceOf<ConcurrentClassA>(ax);
+            Assert.IsNotNull(bx);
+            Assert.IsInstanceOf<ConcurrentClassB>(bx);
+        }
+
+        private static void GenerateJsonForAandB(out string jsonA, out string jsonB)
+        {
+            // set all parameters to false to produce pure JSON
+            fastJSON.JSON.Instance.Parameters = new JSONParameters {EnableAnonymousTypes = false, IgnoreCaseOnDeserialize = false, SerializeNullValues = false, ShowReadOnlyProperties = false, UseExtensions = false, UseFastGuid = false, UseOptimizedDatasetSchema = false, UseUTCDateTime = false, UsingGlobalTypes = false};
+
+            var a = new ConcurrentClassA {PayloadA = new PayloadA()};
+            var b = new ConcurrentClassB {PayloadB = new PayloadB()};
+
+            // A is serialized with extensions and global types
+            jsonA = JSON.Instance.ToJSON(a, new JSONParameters {EnableAnonymousTypes = false, IgnoreCaseOnDeserialize = false, SerializeNullValues = false, ShowReadOnlyProperties = false, UseExtensions = true, UseFastGuid = false, UseOptimizedDatasetSchema = false, UseUTCDateTime = false, UsingGlobalTypes = true});
+            // B is serialized using the above defaults
+            jsonB = JSON.Instance.ToJSON(b);
+
+            Trace.WriteLine(jsonA);
+            Trace.WriteLine(jsonB);
         }
 
         [Test]
         public void UsingGlobalsBug_multithread_nok()
         {
-            var a = new ConcurrentClassA {PayloadA = new PayloadA()};
-            var b = new ConcurrentClassB { PayloadB = new PayloadB() };
-
-            var jsonA = JSON.Instance.ToJSON(a, new JSONParameters { EnableAnonymousTypes = false, IgnoreCaseOnDeserialize = false, SerializeNullValues = false, ShowReadOnlyProperties = false, UseExtensions = true, UseFastGuid = false, UseOptimizedDatasetSchema = false, UseUTCDateTime = false, UsingGlobalTypes = true });
-            var jsonB = JSON.Instance.ToJSON(b);
+            string jsonA;
+            string jsonB;
+            GenerateJsonForAandB(out jsonA, out jsonB);
 
             Console.WriteLine(jsonA);
             Console.WriteLine(jsonB);
 
             object ax=null;
             object bx=null;
+
+            /* Intended timing to force CannotGetType bug in 2.0.5:
+             * the outer class ConcurrentClassA is deserialized first from json with extensions+global types. It reads the global types and sets _usingglobals to true.
+             * The constructor contains a sleep to force parallel deserialization of ConcurrentClassB while in A's constructor.
+             * The deserialization of B sets _usingglobals back to false.
+             * After B is done, A continues to deserialize its PayloadA. It finds type "2" but since _usingglobals is false now, it fails with "Cannot get type".
+             */
+
             var thread = new Thread(() =>
-                           {
-                               bx = JSON.Instance.ToObject(jsonB, typeof(ConcurrentClassB));
-                           });
+                                        {
+                                            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId + " A begins deserialization");
+                                            ax = JSON.Instance.ToObject(jsonA); // A has type information in JSON-extended
+                                            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId + " A is done");
+                                        });
+
             thread.Start();
 
-            ax = JSON.Instance.ToObject(jsonA);
+            Thread.Sleep(500); // wait to allow A to begin deserialization first
 
-            thread.Join();
+            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId + " B begins deserialization");
+            JSON.Instance.ToObject<ConcurrentClassB>(jsonB); // B needs external type info
+            Trace.WriteLine(Thread.CurrentThread.ManagedThreadId + " B is done");
+            
+            thread.Join(); // wait for completion of A due to Sleep in A's constructor
+
             Assert.IsNotNull(ax);
             Assert.IsInstanceOf<ConcurrentClassA>(ax);
             Assert.IsNotNull(bx);
@@ -269,7 +299,9 @@ namespace UnitTests
     {
         public ConcurrentClassA()
         {
+            Trace.WriteLine("ctor ConcurrentClassA. I will sleep for 2 seconds.");
             Thread.Sleep(2000);
+            Trace.WriteLine("ctor ConcurrentClassA. I am done sleeping.");
         }
 
         public PayloadA PayloadA { get; set; }        
@@ -279,12 +311,24 @@ namespace UnitTests
     {
         public ConcurrentClassB()
         {
-            //Thread.Sleep(2000);
+            Trace.WriteLine("ctor ConcurrentClassB.");
         }
         public PayloadB PayloadB { get; set; }
     }
 
-    public class PayloadA{}
-    public class PayloadB{}
+    public class PayloadA
+    {
+        public PayloadA()
+        {
+            Trace.WriteLine("ctor PayLoadA.");
+        }
+    }
+    public class PayloadB
+    {
+        public PayloadB()
+        {
+            Trace.WriteLine("ctor PayLoadB.");
+        }
+    }
 
 }
